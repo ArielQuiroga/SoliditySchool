3e1a4266fa0b7db32e45345ea4bc16b856e98218
unknown <rayder15x@gmail.com>
2023-03-15 01:22:27 -0300

    boton creado y un poco de css

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+       <div className='container'>
+          <button className='myboton'>
+            <p className='buttontext'>Conect to wallet</p>
+          </button>     
+       </div>
+    </div>
+  );
+}
+
+export default App;
