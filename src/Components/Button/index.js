6e17427a86a94001d266df9d3ecb008443db7c5e
unknown <rayder15x@gmail.com>
2023-03-21 10:14:42 -0300

    aprove tokens fixed and working

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -4,27 +4,27 @@ import { ethers } from 'ethers';
 import ERC20 from "../../Contracts/erc20.json"
 import { isContentEditable } from '@testing-library/user-event/dist/utils';
 
+const usdtAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+const usdtABI = ERC20;
 
 function Button() {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const usdtAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-  const usdtABI = ERC20;
-  let usdtContract = new ethers.Contract(usdtAddress, usdtABI, provider);
+  let provider ;
+  let signer;
+  let usdtContract;
   const [isConnected, setIsConnected] = useState(false); // Agrega un estado para controlar si está conectado a MetaMask
   const [isApproved, setIsApproved] = useState(false);
-  let signer;
 
-  //  async function balance() {
-  //    const balance = await usdtContract.balanceOf('0x82E1d4DDd636857Ebcf6a0e74B9b0929C158D7FB');
-  //    console.log (balance.toString())
-  //  }
-  //  balance();
+  const initContract = async () => {
+    usdtContract = new ethers.Contract(usdtAddress, usdtABI, signer);
+    
+  }
 
   const checkConnectState = async () => {
     if (window.ethereum) {
       await window.ethereum.request({ method: 'eth_requestAccounts' });
-      
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
       signer = provider.getSigner();
+      await initContract();
       setIsConnected(true);
     }
   }
@@ -37,6 +37,7 @@ function Button() {
         console.log('Conectado a MetaMask');
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         signer = provider.getSigner();
+        await initContract();
         setIsConnected(true); // Actualiza el estado
       } catch (error) {
         console.error(error);
@@ -49,7 +50,7 @@ function Button() {
   const approveTokens = async () => {
     if (isConnected) {
       try {
-        const tx = await usdtContract.approve( '0x53c902139D9C95a19E809CA39E46269b04739740', 1);
+        const tx = await usdtContract.approve( signer.getAddress(), 1);
         console.log(tx);
         setIsApproved(true);
       } catch (error) {
