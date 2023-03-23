91018eb4064b0c9b43642ea8d5e64662fa7691ad
PatoVerde <leandrodenos@gmail.com>
2023-03-23 08:56:14 -0300

    Finished approve state, go mint state

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Button.css';
 import { ethers } from 'ethers';
 import ERC20 from "../../Contracts/erc20.json"
@@ -6,7 +6,7 @@ import ERC721 from "../../Contracts/erc721.json"
 
 // Debe estar apuntando a polygon testnet
 const usdtAddress = "0xeacdbbfF5808024828D47FCBeaED8Dd2fAfe4A75";
-const tokenNFT = "0x07932903Fcf32802ff951ed75b029821A7dCC8c7"
+const nftAddress = "0x07932903Fcf32802ff951ed75b029821A7dCC8c7"
 const usdtABI = ERC20;
 const nftABI = ERC721;
 
@@ -14,28 +14,19 @@ function Button() {
   let signer;
   let usdtContract;
   let nftContract;
+  const [approveText, setApproveText] = useState('Approve');
   const [isConnected, setIsConnected] = useState(false); // Agrega un estado para controlar si está conectado a MetaMask
-  const [isApproved, setIsApproved] = useState(false);
+  const [isApproved, setIsApproved] = useState('');
 
   const initContract = async () => {
     usdtContract = new ethers.Contract(usdtAddress, usdtABI, signer);
-    nftContract = new ethers.Contract(nftContract, nftABI, signer);
-    
+    nftContract = new ethers.Contract(nftAddress, nftABI, signer);    
   }
+
+  useEffect(() => {
+    connectWallet()
+  }, [])
   
-
-
-  const checkConnectState = async () => {
-    if (window.ethereum) {
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      signer = provider.getSigner();
-      await initContract();
-      setIsConnected(true);
-    }
-  }
-  checkConnectState();
-
   const connectWallet = async () => {
     if (window.ethereum) {
       try {
@@ -44,6 +35,7 @@ function Button() {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         signer = provider.getSigner();
         await initContract();
+        allowanceTokens();
         setIsConnected(true); // Actualiza el estado
       } catch (error) {
         console.error(error);
@@ -52,11 +44,23 @@ function Button() {
       console.error('MetaMask no está instalado');
     }
   };
-
+  
   const approveTokens = async () => {
     if (isConnected) {
       try {
-        const tx = await usdtContract.approve( tokenNFT, '9999000000000000000000');
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        signer = provider.getSigner();
+        await initContract();
+
+        const tx = await usdtContract.approve( nftAddress, '9999000000000000000000', {
+          gasLimit: 300000,
+        });
+        setApproveText('Approving...')
+
+        await tx.wait().then(() =>{
+          setApproveText('Approve');
+        })
+
         console.log(tx);
         setIsApproved(true);
       } catch (error) {
@@ -67,13 +71,31 @@ function Button() {
     }
   };
 
+  const allowanceTokens = async () => {
+    const allowance = await usdtContract.allowance(signer.getAddress(), nftAddress );
+    if(allowance > ethers.utils.formatEther(1)){
+      setIsApproved(true);
+    }else{
+      setIsApproved(false);
+    }
+  }
 
+  const mint = async () => {
+    console.log('hola');
+  }
+  
   return (
     <div className="App">
-      <div className='container'>
-        <button className='myboton' onClick={isConnected ? approveTokens : connectWallet }>
-          <p className='buttontext'>{isConnected ? 'Approve' : 'Connect to wallet'}</p> 
-        </button>     
+      <div className='container'>   
+
+        <button className='myboton' onClick={
+          isConnected 
+          ? (isApproved ? mint : approveTokens) 
+          : connectWallet 
+        }>
+          <p className='buttontext'>{isConnected ? (isApproved ? 'Mint' : approveText) : 'Connect to wallet'}</p> 
+        </button>
+
       </div>
     </div>
   );
